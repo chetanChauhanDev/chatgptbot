4a5e455eb009c53a9d269769d647f1743c25d2ca
chetan chauhan <chetanrajputsm@gmail.com>
2023-03-10 11:57:10 +0530

    code deploye

--- src/pages/api/hello.js
+++ src/pages/api/hello.js
@@ -1,5 +1,5 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
-
+// Next.js API route support: https://nextjs.org/docs/api-routes/introductionjkkjsjkdgsfjksdgjfksdk
+//its testing
 export default function handler(req, res) {
   res.status(200).json({ name: 'John Doe' })
 }
